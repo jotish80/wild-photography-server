47a7e10696feb52dc401a15cf24817347154b255
jotish80 <sjotishchandra@gmail.com>
2022-11-10 16:29:16 +0100

    review delete api added

--- index.js
+++ index.js
@@ -40,10 +40,45 @@ async function run () {
         });
 
         //review api
+          app.patch('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set:{
+                    status: status
+                }
+            }
+            const result = await reviewCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
+         app.get('/reviews', async (req, res) => {
+            let query = {};
+
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+
+            const cursor = reviewCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review);
+        });
+
+
         app.post('/reviews', async(req,res) =>{
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result)
+        });
+
+           app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally{
